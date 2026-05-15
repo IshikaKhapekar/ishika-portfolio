629f17cf711b4147980d9e80bb2eb87bc264ec9c
Ishika Khapekar <ishikakhapekar6@gmail.com>
2026-05-15 16:33:45 +0530

    Update vite.config.ts

--- ishika-s-data-canvas-main/vite.config.ts
+++ ishika-s-data-canvas-main/vite.config.ts
@@ -1,14 +1,11 @@
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import { defineConfig } from 'vite'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/react-start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [
-    tsConfigPaths(),
-    tanstackStart({
-      server: {
-        preset: 'vercel',
-      },
-    }),
-  ],
-})
+  server: {
+    preset: "vercel",
+  },
+  vite: {
+    plugins: [tsConfigPaths()],
+  },
+});
